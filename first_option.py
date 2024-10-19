84309315f3897eea6d905193845a6fc0a7b19816
ivan2330 <ivankozhevnyk@gmail.com>
2024-10-20 00:44:52 +0300

    first commit

---
+++ first_option.py
@@ -0,0 +1,64 @@
+"""Here I want to show the option to solve the task through an abstract class in Python"""
+
+import json
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod 
+    def get_sound(self):
+        pass
+    
+class Dog(Animal):
+    def get_sound(self):
+        return "bark"
+    
+class Cat(Animal):
+    def get_sound(self):
+        return "meow"
+    
+class Cow(Animal):
+    def get_sound(self):
+        return "moo"
+    
+class Rat(Animal):
+    def get_sound(self):
+        return "pipi"
+    
+class Alien(Animal):
+    def get_sound(self):
+        return "KILL"
+    
+class AnimalGroup:
+    @staticmethod
+    def create_animal(animal_type):
+        animals = {
+            "dog": Dog(),
+            "cat": Cat(),
+            "cow": Cow(),
+            "rat": Rat(),
+            "alien": Alien(),
+        }
+        return animals.get(animal_type, None)
+    
+def main():
+    try:
+        with open("test.json", "r") as file:
+            data = json.load(file)
+            
+            if data.get("field1") != "value1":
+                print("Invalid input: field1 must be 'value1")
+                
+        animal_type = data.get("animal")
+        animal = AnimalGroup.create_animal(animal_type)
+        
+        if animal:
+            print(animal.get_sound())
+        else:
+            print("Unknown animal")
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error reading file {e}")
+        
+if __name__=="__main__":
+    main()
+            
+            
---
+++ second_option.py
@@ -0,0 +1,30 @@
+"""Here I want to show the option to solve the task through simple functions in Python"""
+
+import json
+
+def read_animal_sound(file_path):
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    
+    
+    if data.get("field1") != "value1":
+        return "Invalid data: field1 must be 'value1'"
+    
+    if set(data.keys()) != {"field1", "animal"}:
+        return "Invalid data: Only 'field1' and 'animal' fields are allowed"      
+    
+    sounds = {
+        "dog": "bark",
+        "cat": "meow",
+        "cow": "moo",
+        "rat": "pipi",
+        "alien": "KILL"
+    }
+
+    animal = data.get("animal", "")
+
+    return sounds.get(animal, "Unknown sound")
+
+
+if __name__ == "__main__":
+    print(read_animal_sound(file_path="right directory"))
